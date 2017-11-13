892d01de9eedb109c47afe31ea981ec31cd95055
nicks189 <nsteger@iastate.edu>
2017-11-13 12:56:18 -0600

    remove redundant datepicker function

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-    $('#datepicker').datepicker({
-        dateFormat: 'yy-mm-dd', minDate: 0
-    });
     $('#datetimepicker1').datetimepicker({
         format: 'YYYY-MM-DD HH:mm:ss'
     });
