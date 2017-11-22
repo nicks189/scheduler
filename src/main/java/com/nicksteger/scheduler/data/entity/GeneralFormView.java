e138d2d100a8f50441836777b27e33b9185ef0a6
nicks189 <nsteger@iastate.edu>
2017-11-21 21:18:02 -0600

    Can now pick events by date using jquery calendar

---
+++ src/main/java/com/nicksteger/scheduler/data/entity/GeneralFormView.java
@@ -0,0 +1,13 @@
+package com.nicksteger.scheduler.data.entity;
+
+public class GeneralFormView {
+    private String formText;
+
+    public String getFormText() {
+        return formText;
+    }
+
+    public void setFormText(String formText) {
+        this.formText = formText;
+    }
+}
