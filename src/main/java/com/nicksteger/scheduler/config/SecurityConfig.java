9f03ddb3391114156ee26005d55ab06b60ca0c25
nicks189 <nsteger@iastate.edu>
2017-12-05 13:44:31 -0600

    SecurityConfig testing

--- src/main/java/com/nicksteger/scheduler/config/SecurityConfig.java
+++ src/main/java/com/nicksteger/scheduler/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.nicksteger.scheduler.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,4 +13,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
     }
+
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    //     http.authorizeRequests()
+    //             .requestMatchers(StaticResourceRequest.toCommonLocations()).permitAll()
+    //             .anyRequest().fullyAuthenticated()
+    //             .and()
+    //             .formLogin().loginPage("/login").failureUrl("/login?error").permitAll()
+    //             .and()
+    //             .logout().permitAll();
+    // }
 }
