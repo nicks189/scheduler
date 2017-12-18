fc54295ff8bc70ffc722d0d5683780c25ac40083
nicks189 <nsteger@iastate.edu>
2017-12-17 19:34:05 -0600

    added UserDetails and update security config

---
+++ src/main/java/com/nicksteger/scheduler/converters/UserToUserDetails.java
@@ -0,0 +1,37 @@
+package com.nicksteger.scheduler.converters;
+
+import com.nicksteger.scheduler.data.entity.User;
+import com.nicksteger.scheduler.data.entity.UserRole;
+import com.nicksteger.scheduler.service.UserRoleService;
+import com.nicksteger.scheduler.service.security.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class UserToUserDetails implements Converter<User, UserDetails> {
+    @Autowired
+    private UserRoleService userRoleService;
+
+    @Override
+    public UserDetails convert(User user) {
+        UserDetailsImpl userDetails = new UserDetailsImpl();
+        if (user != null) {
+            userDetails.setUsername(user.getUsername());
+            userDetails.setPassword(user.getPassword());
+            Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+            List<UserRole> userRoles = this.userRoleService.getUserRolesByUserId(user.getId());
+            for (UserRole userRole : userRoles) {
+                authorities.add(new SimpleGrantedAuthority(userRole.getRole()));
+            }
+            userDetails.setAuthorities(authorities);
+        }
+        return userDetails;
+    }
+}
