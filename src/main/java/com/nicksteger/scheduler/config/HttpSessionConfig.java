e200dbf3daee3a024bd2d9629018c9cf5849e698
nicks189 <nsteger@iastate.edu>
2018-01-06 15:27:59 -0600

    Unsuccesful ElastiCache Redis Cluster config

--- src/main/java/com/nicksteger/scheduler/config/HttpSessionConfig.java
+++ src/main/java/com/nicksteger/scheduler/config/HttpSessionConfig.java
@@ -1,7 +1,13 @@
 package com.nicksteger.scheduler.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
 public class HttpSessionConfig {
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
 }
