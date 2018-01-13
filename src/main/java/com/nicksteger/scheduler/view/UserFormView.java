ef8746e51090f84a1a76d32f028d8d3a363c012c
nicks189 <nsteger@iastate.edu>
2018-01-12 21:16:02 -0600

    Add UserFormView class

---
+++ src/main/java/com/nicksteger/scheduler/view/UserFormView.java
@@ -0,0 +1,49 @@
+package com.nicksteger.scheduler.view;
+
+public class UserFormView {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String confirmedPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmedPassword() {
+        return confirmedPassword;
+    }
+
+    public void setConfirmedPassword(String confirmedPassword) {
+        this.confirmedPassword = confirmedPassword;
+    }
+}
